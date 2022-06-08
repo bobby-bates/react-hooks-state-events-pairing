b8bd564988697106471ecc30c96e0de71f25ae74
Bobby Bates <robertfbatesjr@gmail.com>
2022-06-08 10:37:25 -0400

    add state for data/video.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,35 @@
+import { useState } from 'react'
 import video from "../data/video.js";
 
 function App() {
-  console.log("Here's your data:", video);
+  const { 
+    id,
+    title,
+    embedUrl,
+    views,
+    createdAt,
+    upvotes,
+    downvotes,
+    comments } = video
+
+  console.log("Here's your data:");
+  // console.log(video);
+  console.log(id);
+  console.log(title);
+  console.log(embedUrl);
+  console.log(views);
+  console.log(createdAt);
+  console.log(upvotes);
+  console.log(downvotes);
+  console.log(comments);
+  /*
+  State needed:
+  - Comments (arr of objs)
+  - Upvotes count (int)
+  - Downvotes count (int)
+  */
+
+  const [currUpvotes, setCurrUpvotes] = useState()
 
   return (
     <div className="App">
